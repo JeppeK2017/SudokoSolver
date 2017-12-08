1ed55c802948be9cd4b519ca344428fcc50950e8
Jeppe Kristensen <jeppe_kuk@hotmail.com>
2017-12-08 10:15:51 +0100

    Added table from file Added partial legal method

--- src/Solver.py
+++ src/Solver.py
@@ -1,21 +1,41 @@
 # List containg the table
 
-table = [[None] * 9] * 9
-
+import os
 
 try:
-    fhand = open('sudokus/Sudoko1.txt')
+    lines = open('sudokus/Sudoko1.txt').read().splitlines()
 except:
     print("File does not exist")
     exit()
-
-
-i = 0
-for line in fhand:
-    for j in range(0, 9):
-        table[i][j] = 0
-    i = i + 1
+table = lines
 
 
 
-print(table)
+def findLegal(table, posI, posJ):
+    print("Find legal method")
+    test = int(table[posI][posJ])
+
+    possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    if test == 0:
+        #Rows
+        for j in range(0, 9):
+            if j != posJ:
+                val = int(table[posI][j])
+                if val != 0:
+                    possibleNumbers.remove(val)
+
+        #Columns
+        for i in range(0, 9):
+            if i != posI:
+                val = int(table[i][posJ])
+                if val != 0:
+                    possibleNumbers.remove(val)
+
+        # 3x3 square
+
+    return possibleNumbers
+
+
+legalNumbers = findLegal(table, 0, 0)
+print(legalNumbers)
